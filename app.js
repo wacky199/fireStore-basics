6f1074e66ecea6a41545dc391508752d03f859aa
wacky <shivamsingh01111999@gmail.com>
2021-07-09 13:33:11 +0530

    app is ready to fetch data form db to client site

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const cafeList=document.querySelector('#cafe-list');
+const cafeList = document.querySelector('#cafe-list');
 
 //create element and render cafe
+const renderCafe = (doc) => {
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let city = document.createElement('span');
+    li.setAttribute('data-id', doc.id);
+    name.textContent = doc.data().name;
+    city.textContent = doc.data().city;
+    li.appendChild(name);
+    li.appendChild(city);
+
+    cafeList.appendChild(li);
+};
 
 db.collection('listedCafes')
     .get()
     .then((snapshot) => {
         snapshot.docs.forEach((doc) => {
-            console.log(doc.data());
+            renderCafe(doc);
         });
     });
